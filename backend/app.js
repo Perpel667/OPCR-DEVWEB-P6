7c4a3310370c9e608fc9e56c0ce103c26160cee7
Lucas Oliveira <purzbbq@gmail.com>
2021-10-15 11:21:47 +0200

    define headers

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,16 @@ const express = require('express');
 
 const app = express();
 
+// definition des headers
+app.use((req, res, next) => {
+    // nous permet d'acceder a l'API depuis n'importe quelle origine
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // nous permet d'ajouter les headers mentionnés aux requetes envoyés a l'API
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    // nous permet d'envoyer des requetes avec les methodes mentionnées
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
 
 // exporte le module app.js
 module.exports = app;
